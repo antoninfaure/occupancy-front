865f1253f0c0a1d5e2b9af24087cdc93163d6521
Antonin <antonin.faure@epfl.ch>
2024-01-12 01:35:02 +0100

    vitejs & package config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_PORT } = env
+
+  return defineConfig({
+    base: '/',
+    plugins: [react()],
+    server: {
+      port: +VITE_PORT || 3000,
     },
-  },
-})
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  })
+
+}
